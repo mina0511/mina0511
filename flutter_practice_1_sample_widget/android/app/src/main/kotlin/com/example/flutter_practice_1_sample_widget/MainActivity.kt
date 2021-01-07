9310fb86c1f3d06add118c306fbab6247ca550ad
Mina Kang <kasong13@unist.ac.kr>
2021-01-08 04:09:13 +0900

    2021-01-08
    -practice about "오준석의 플러터 생존코딩"
    - 117pg

---
+++ flutter_practice_1_sample_widget/android/app/src/main/kotlin/com/example/flutter_practice_1_sample_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_practice_1_sample_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_practice_1_sample_widget/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart'; //material디자인 위젯을 사용하기 위한 import
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // 상태가 없는 위젯을 정의하는데 사용(한번그려진후 다시 그려지지 않음을 의미)
+  @override
+  Widget build(BuildContext context) {
+    //build함수는 실제 화면에 그릴 위젯을 작성하여 반환함
+    return MaterialApp(
+      title: 'Hello World ',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Hello World'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  //상태가 있는 위젯 정의
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _text = 'Hello';
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Hello World'),
+      ),
+      body: Text(
+        _text,
+        style: TextStyle(fontSize: 40),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _text = 'World';
+          });
+        },
+        child: Icon(Icons.touch_app),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
