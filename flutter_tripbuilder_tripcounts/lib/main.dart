fbf225a29a2b5546c0667a60bb1b757a06b28ddf
Mina Kang <kasong13@unist.ac.kr>
2021-01-31 01:41:23 +0900

    tripbulider_widget
    -Count the number of travel plans

--- flutter_tripbuilder_tripcounts/lib/main.dart
+++ flutter_tripbuilder_tripcounts/lib/main.dart
@@ -50,68 +50,176 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final _valueList = ['0', '1', '2', '3', '4', '5'];
+  var _selectedValue = '0';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40),
+            border: Border.all(color: Colors.greenAccent[100], width: 3),
+            color: Colors.greenAccent[100]),
+        width: 320,
+        height: 300,
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.access_time_rounded, size: 30),
+                SizedBox(
+                  width: 1,
+                ),
+                Text("아침 전",
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                SizedBox(width: 108),
+                Container(
+                  color: Colors.white,
+                  child: DropdownButton(
+                    iconEnabledColor: Colors.grey,
+                    focusColor: Colors.white,
+                    dropdownColor: Colors.white,
+                    value: _selectedValue,
+                    items: _valueList.map(
+                      (value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Text('         ' + value + '      '),
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedValue = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          SizedBox(height: 8),
+          Container(height: 1, width: 320, color: Colors.white),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.access_time_rounded, size: 30),
+                SizedBox(
+                  width: 1,
+                ),
+                Text("아침과 점심 사이",
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                SizedBox(width: 50),
+                Container(
+                  color: Colors.white,
+                  child: DropdownButton(
+                    iconEnabledColor: Colors.grey,
+                    focusColor: Colors.white,
+                    dropdownColor: Colors.white,
+                    value: _selectedValue,
+                    items: _valueList.map(
+                      (value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Text('         ' + value + '      '),
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedValue = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 8),
+          Container(height: 1, width: 320, color: Colors.white),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.access_time_rounded, size: 30),
+                SizedBox(
+                  width: 1,
+                ),
+                Text("점심과 저녁 사이",
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                SizedBox(width: 50),
+                Container(
+                  color: Colors.white,
+                  child: DropdownButton(
+                    iconEnabledColor: Colors.grey,
+                    focusColor: Colors.white,
+                    dropdownColor: Colors.white,
+                    value: _selectedValue,
+                    items: _valueList.map(
+                      (value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Text('         ' + value + '      '),
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedValue = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 8),
+          Container(height: 1, width: 320, color: Colors.white),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.access_time_rounded, size: 30),
+                SizedBox(
+                  width: 1,
+                ),
+                Text("저녁 이후",
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                SizedBox(width: 97),
+                Container(
+                  color: Colors.white,
+                  child: DropdownButton(
+                    iconEnabledColor: Colors.grey,
+                    focusColor: Colors.white,
+                    dropdownColor: Colors.white,
+                    value: _selectedValue,
+                    items: _valueList.map(
+                      (value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Text('         ' + value + '      '),
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedValue = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
